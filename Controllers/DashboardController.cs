f1f01c0078483bbaeefc1f1416aeda3f3a6ea45a
New Light <darthmiles24@gmail.com>
2025-03-12 22:30:50 +0100

    Utworzenie kontrolera odpowiedzialnego za obsługę zalogowanych użytkowników.

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kliker.Controllers
+{
+    public class DashboardController : Controller
+    {
+        [Authorize]
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
